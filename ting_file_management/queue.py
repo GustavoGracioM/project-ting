205ca8d6f1b5ae7476b1cd353c3a2bcd4eac0e77
GustavoGracioM <gustavogracio@hotmail.com>
2022-12-01 15:03:55 -0300

    implementa classe Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 1 and index == -1:
+            raise IndexError('deque index out of range')
+        return self._data[index]
