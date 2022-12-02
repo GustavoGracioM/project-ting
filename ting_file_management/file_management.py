79a1e05420e3bccafcd71277c9ea477bb0fb5aab
GustavoGracioM <gustavogracio@hotmail.com>
2022-12-01 22:03:18 -0300

    implementa função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,21 @@
+import os
+import sys
+
+
+def is_validate_format_txt(path_file):
+    _, format = os.path.splitext(path_file)
+    if format != '.txt':
+        return False
+    return True
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    is_validated = is_validate_format_txt(path_file)
+    if is_validated:
+        try:
+            with open(path_file) as f:
+                return f.read().split("\n")
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        print('Formato inválido', file=sys.stderr)
