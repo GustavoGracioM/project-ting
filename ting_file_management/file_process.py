ec7390a54997b8d89bc8284aa10d9347db8142db
GustavoGracioM <gustavogracio@hotmail.com>
2022-12-02 01:03:24 -0300

    implementa função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def is_exits_file(path_file: str, instance: Queue):
+    for i in range(len(instance)):
+        if path_file == instance.search(i)['nome_do_arquivo']:
+            return True
+    return False
+
+
+def process(path_file: str, instance: Queue):
+    exits = is_exits_file(path_file, instance)
+    if not exits:
+        lines_txt = txt_importer(path_file)
+        process_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines_txt),
+            "linhas_do_arquivo": lines_txt
+        }
+        instance.enqueue(process_dict)
+        print(process_dict, file=sys.stdout)
 
 
 def remove(instance):
