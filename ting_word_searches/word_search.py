f32592b8906ca8c814745a92de11a764e4dbb69f
GustavoGracioM <gustavogracio@hotmail.com>
2022-12-02 14:53:31 -0300

    implementa função search_by_word e refatora função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,27 +1,41 @@
 from ting_file_management.queue import Queue
 
 
-def ocorrencias_in_lines(lines, word):
-    pp = []
+def occurrences_exists(lines, word):
+    result = []
     for s in range(len(lines)):
         if word in lines[s].lower():
-            pp.append({"linha": s + 1})
-    return pp
+            result.append({"linha": s + 1})
+    return result
+
+
+def occurrences_search(lines, word):
+    result = []
+    for s in range(len(lines)):
+        if word in lines[s].lower():
+            result.append({"linha": s + 1, "conteudo": lines[s]})
+    return result
+
+
+def occurrences(word: str, instance: Queue, type_occurrences):
+    list_occurrences = []
+    for i in range(len(instance)):
+        lines = instance.search(i)["linhas_do_arquivo"]
+        content_lines = type_occurrences(lines, word)
+        if len(content_lines) > 0:
+            list_occurrences.append(
+                {
+                    "palavra": word,
+                    "arquivo": instance.search(i)["nome_do_arquivo"],
+                    "ocorrencias": content_lines,
+                }
+            )
+    return list_occurrences
 
 
 def exists_word(word: str, instance: Queue):
-    list_ocorrencias = []
-    for i in range(len(instance)):
-        lines = instance.search(i)['linhas_do_arquivo']
-        content_lines = ocorrencias_in_lines(lines, word)
-        if len(content_lines) > 0:
-            list_ocorrencias.append({
-                "palavra": word,
-                "arquivo": instance.search(i)['nome_do_arquivo'],
-                "ocorrencias": content_lines
-            })
-    return list_ocorrencias
+    return occurrences(word, instance, occurrences_exists)
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return occurrences(word, instance, occurrences_search)
